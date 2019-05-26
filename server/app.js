3fbaf99dbd9d6a4c99fffee7b7130e286682e593
marco_dca <marcodecara@gmail.com>
2019-05-26 23:29:35 +0200

    conection to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//OBS! need to take this data into .env
+mongoose.connect('mongodb+srv://admin:reffen123@cluster0-nvtf4.mongodb.net/test?retryWrites=true');
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
+
 
 //Single endpoint for graphql
 app.use('/graphql', graphqlHTTP({
