6fee12acb26430b07275bc4f9e4c9f03187e40b7
marco_dca <marcodecara@gmail.com>
2019-05-28 11:02:22 +0200

    attempt to setup full-stack

--- server/app.js
+++ server/app.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
-const mongoose = require('mongoose');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
 //OBS! need to take this data into .env
-mongoose.connect('mongodb+srv://admin:reffen123@cluster0-nvtf4.mongodb.net/test?retryWrites=true');
-mongoose.connection.once('open', () => {
-    console.log('connected to database')
-})
+mongoose.connect(
+  "mongodb+srv://admin:reffen123@cluster0-nvtf4.mongodb.net/test?retryWrites=true"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 //Single endpoint for graphql
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql : true
-}))
+    graphiql: true
+  })
+);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, ()=>{
-    console.log(`Now listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
