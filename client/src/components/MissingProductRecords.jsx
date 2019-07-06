ed3c3ffb81437efce84e41ce251470455033de6a
marco_dca <marcodecara@gmail.com>
2019-07-06 15:10:48 +0200

    .

--- client/src/components/MissingProductRecords.jsx
+++ client/src/components/MissingProductRecords.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { graphql, compose } from "react-apollo";
-import { getProductsQuery, getMissingProductsRecordsQuery, deleteMissingProductRecord } from "../queries";
+import {
+  getProductsQuery,
+  getMissingProductsRecordsQuery,
+  deleteMissingProductRecord
+} from "../queries";
 import styled from "styled-components/macro";
+import media from "../styles/media";
+
+
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-
-import media from "../styles/media";
+import IconButton from "@material-ui/core/IconButton";
+import MoveToInbox from "@material-ui/icons/MoveToInbox";
 
 const Container = styled.div`
   width: 100%;
@@ -19,24 +26,22 @@ const MissingProductCard = styled(Paper)`
   width: 90%;
   padding: 3%;
   margin: 4% auto;
-`
+`;
 
 const MissingProductRecords = ({
   products: { products },
   missingProductRecords: { missingProductRecords },
   deleteMissingProductRecord
 }) => {
-
-  const handleDeleteMissingProductRecord = (id) => {
-
+  const handleDeleteMissingProductRecord = id => {
     deleteMissingProductRecord({
       variables: {
-        missingProductRecordId : id
+        missingProductRecordId: id
       },
       //refetch the get Missing Product Records query
       refetchQueries: [{ query: getMissingProductsRecordsQuery }]
-    })
-  }
+    });
+  };
 
   return (
     <>
@@ -45,17 +50,38 @@ const MissingProductRecords = ({
           missingProductRecords.map(missingProduct => (
             <MissingProductCard key={missingProduct.id}>
               {/* {console.log(missingProduct)} */}
-              <Typography variant="h6">{missingProduct.product.name}</Typography>
-              <Typography variant='subtitle2'>
-                Missing since: {missingProduct.dateAdded}
-                <button
-                  onClick={()=>{
-                    handleDeleteMissingProductRecord(missingProduct.id)
-                  }}
+              <Typography variant="h6">
+                {missingProduct.product.name}
+              </Typography>
+              <Typography variant="subtitle2">
+                <div
+                  css={`
+                    margin-top: 1%;
+                    display: flex;
+                    justify-content: space-between;
+                  `}
                 >
-                  Delete missing product record
-                </button>
-              </Typography>         
+                  <Typography variant="subtitle1">
+                    <b>Missing since:</b> {missingProduct.dateAdded}
+                  </Typography>
+
+                  {missingProduct.comment && (
+                    <Typography variant="subtitle1">
+                      <b>Comment:</b> {missingProduct.comment}
+                    </Typography>
+                  )}
+
+                    <MoveToInbox/>
+
+                  {/* <button
+                    onClick={() => {
+                      handleDeleteMissingProductRecord(missingProduct.id);
+                    }}
+                  >
+                    Delete missing product record
+                  </button> */}
+                </div>
+              </Typography>
             </MissingProductCard>
           ))}
       </Container>
@@ -66,5 +92,5 @@ const MissingProductRecords = ({
 export default compose(
   graphql(getProductsQuery, { name: "products" }),
   graphql(getMissingProductsRecordsQuery, { name: "missingProductRecords" }),
-  graphql(deleteMissingProductRecord, {name : 'deleteMissingProductRecord'})
+  graphql(deleteMissingProductRecord, { name: "deleteMissingProductRecord" })
 )(MissingProductRecords);
